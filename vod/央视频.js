fde26985cb9c63420fae5ef5eb1d20574aca16d6
Jonas Lin <lemon52030@gmail.com>
2026-01-17 01:21:34 +0800

    取代 央视频.js

--- vod/央视频.js
+++ vod/央视频.js
@@ -1,18 +1,14 @@
 /**
- * 央视频 720P
+ * 央视频 1080P
  * 作者：deepseek
  * 版本：1.0
  * 最后更新：2026-1-2 02:46:31
  * 发布页 https://m.yangshipin.cn/
  * @config
- * debug: false
- * blockList: *.[ico|png|jpeg|jpg|gif|webp]*|*.css
+ * debug: true
+ * blockList: *.[ico|png|jpeg|jpg|gif|webp]*
  */
 
-const baseUrl = 'https://m.yangshipin.cn';
-const headers = {
-	'user-agent': 'Mozilla/5.0 (Linux; Android 12; HarmonyOS; ELS-AN10; HMSCore 6.11.0.302) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.88 HuaweiBrowser/13.0.3.320 Mobile Safari/537.36'
-};
 
 /**
  * 初始化配置
@@ -37,8 +33,9 @@ async function homeContent(filter) {
  * 首页推荐视频
  */
 async function homeVideoContent() {
-    Java.wvOpen(`${baseUrl}/tv?pid=600001859`);
-    return { list: [] };
+    return {
+        list: []
+    };
 }
 
 /**
@@ -65,6 +62,7 @@ async function categoryContent(tid, pg, filter, extend) {
                 vod_id: item.pid,
                 vod_name: item.channelName,
                 vod_pic: vodPic,
+                action: item.pid,
                 style: { type: "rect", ratio: isCentral ? 1.66 : 1 }
             };
         })
@@ -85,97 +83,37 @@ async function categoryContent(tid, pg, filter, extend) {
  * 详情页
  */
 async function detailContent(ids) {
-    const res = await Java.req(`https://h5access.yangshipin.cn/web/h5_live_poll?raw=1&pid=${ids[0]}&reqType=2`);
-    const jsonData = JSON.parse(res.body);
-    const epgs = jsonData?.data?.h5TVLivePollRsp?.tvLivePollRsp?.programs || [];
-    const currentTime = jsonData?.data?.h5TVLivePollRsp?.tvLivePollRsp?.currentServerTime || 0;
-    
-    const dayStart = Math.floor(currentTime / 86400) * 86400;
-    let liveName = '';
-    let replayList = [];
+    return {
+        list: []
+    };
+}
 
-    epgs.forEach(item => {
-        const endTime = item.start_time_stamp + item.duration;
-        if (currentTime >= item.start_time_stamp && currentTime <= endTime) {
-            liveName = item.name;
-        }
-        if (item.start_time_stamp >= dayStart && item.start_time_stamp < currentTime) {
-			let epg_time = formatTime(item.start_time_stamp, 'MM-DD HH:mm');
-            replayList.push(`[${epg_time}]${item.name}$${ids[0]}_${item.start_time_stamp}_${item.id}`);
+async function action(pid) {
+    Java.wvPlayerSetAllowInteraction(false); // 是否允许操作, true为允许.会禁用音量手势
+    Java.wvPlayerSetScale(0); //画面比例: 0=默认, 1=16:9, 2=4:3, 3=填充, 4=原始, 5=裁剪
+    // Java.wvPlayerSetVolume(90); // 初始音量
+    Java.wvPlayer({
+        url: 'https://yangshipin.cn/tv/home?pid=' + pid,
+        headers: {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
         }
     });
 
-    const list = [{
-        vod_id: ids[0],
-        vod_name: jsonData?.data?.h5TVLivePollRsp?.tvLivePollRsp?.title,
-        vod_content: jsonData?.data?.h5TVLivePollRsp?.tvLivePollRsp?.title + `【${liveName}】正在直播中 `,
-        vod_play_from: '正在直播$$$节目回看',
-        vod_play_url: `${liveName}$${ids[0]}$$$${replayList.join('#')}`
-    }];
-    return { code: 1, msg: "数据列表", page: 1, pagecount: 1, limit: 1, total: 1, list };
 }
+
 
 /**
  * 搜索
  */
 async function searchContent(key, quick, pg) {
-    return { list: [] };
+    return {
+        list: []
+    };
 }
 
 /**
  * 播放器
  */
 async function playerContent(flag, id, vipFlags) {
-	//Java.showWebView();
-	let playUrl = `https://m.yangshipin.cn/tv?pid=${id}&ptag=4_1.0.5.15187_copy,4_1.0.0.20034_copy`;
-	if (flag == '节目回看') {
-		const ids = id.split("_"); 
-		const res = await Java.req(`https://h5access.yangshipin.cn/web/h5_share?vappid=&vsecret=b42702bf7309a179d102f3d51b1add2fda0bc7ada64cb801&raw=1&shareToType=1&shareId=itemtype%3Dcommon%26shareType%3Dtv%26pid%3D${ids[0]}%26startTime%3D${ids[1]}%26programID%3D${ids[2]}&shareFrom=h5`);
-		const jsonData = JSON.parse(res.body);
-		playUrl = jsonData.data.shareUrl;
-	}
-    return {
-        type: 'sniff',
-        url: playUrl,
-		headers: headers,
-        script: `let t=setInterval(()=>{let p=document.querySelector("#v-live-video");if(p?.play){p.play();clearInterval(t)}},100);`,
-        timeout: 15
-    };
-}
-
-
-function formatTime(timestamp, format = 'YYYY-MM-DD HH:mm:ss') {
-    const date = new Date(timestamp * 1000);
-    const map = {
-        'YYYY': date.getFullYear(),
-        'MM': String(date.getMonth() + 1).padStart(2, '0'),
-        'DD': String(date.getDate()).padStart(2, '0'),
-        'HH': String(date.getHours()).padStart(2, '0'),
-        'mm': String(date.getMinutes()).padStart(2, '0'),
-        'ss': String(date.getSeconds()).padStart(2, '0')
-    };
-    
-    return format.replace(/YYYY|MM|DD|HH|mm|ss/g, matched => map[matched]);
-}
-
-
-if (typeof window.hasCheckedLogin === 'undefined') {
-    window.hasCheckedLogin = true;
-    
-    let cookies = Java.getCookies('https://m.yangshipin.cn');
-
-    if (!cookies || !cookies.includes('vuserid')) {
-        let confirmed = Java.showConfirmDialog('提示', '央视频已限制登录后才可以观看，点击确定后请自行登录！');
-        
-        if (confirmed) {
-            let t = setInterval(() => {
-                let p = document.querySelector("#v-live-video");
-                if (p?.play) {
-                    p.play();
-                    Java.showWebView();
-                    clearInterval(t);
-                }
-            }, 100);
-        }
-    }
+    return;
 }
