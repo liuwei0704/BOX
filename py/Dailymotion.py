d75b8cc9084056e571eb36baa20c9395df75d1c4
Jonas Lin <lemon52030@gmail.com>
2026-06-06 18:24:20 +0800

    取代 Dailymotion.py

--- py/Dailymotion.py
+++ py/Dailymotion.py
@@ -17,6 +17,8 @@ class Spider(Spider):
     def homeContent(self, filter):
         cate_list = [
             {"type_name": "短劇: narumi4001 ", "type_id": "user/narumi4001"},
+            {"type_name": "短劇: douyinduanju ", "type_id": "user/douyinduanju"},
+            {"type_name": "短劇: DailyCDrama ", "type_id": "user/DailyCDrama"},
             {"type_name": "短劇: ckhow9393-528 ", "type_id": "user/ckhow9393-528"},
             {"type_name": "短劇: zenostar ", "type_id": "user/zenostar"},
             {"type_name": "短劇: huinan520-349", "type_id": "user/huinan520-349"},
@@ -25,7 +27,6 @@ class Spider(Spider):
             {"type_name": "短劇: drkr004", "type_id": "user/drkr004"},
             {"type_name": "短劇: yi.tong292", "type_id": "user/yi.tong292"},
             {"type_name": "短劇: kchow125 ", "type_id": "user/kchow125"},
-            {"type_name": "短劇: fddchannel", "type_id": "user/fddchannel"},
             {"type_name": "短劇: Cabian", "type_id": "user/cabian"},
             {"type_name": "新聞", "type_id": "tag/news"},
             {"type_name": "體育", "type_id": "tag/sport"},
